d926e9d1ae9728f287e587ddbd7985f5b504ccb8
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-21 17:06:50 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,4 @@
+ variable "region" { default = "us-east-1"}
+ variable "main_vpc_cidr" { default = "10.0.0.0/24"}
+ variable "public_subnets" { default = "10.0.0.128/26"}
+ variable "private_subnets" { default = "10.0.0.192/26"}
