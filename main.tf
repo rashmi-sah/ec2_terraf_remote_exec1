d326c5dc972bd7fd481f308335c6194b82e03e58
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-21 17:13:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,28 @@ resource "aws_instance" "aws_ec_instance" {
   associate_public_ip_address = true
   key_name = "AWS_EC2_key"
 
+  connection {
+    type="ssh"
+    user = "ubuntu"
+    host=self.public_ip
+    private_key=file("/home/runner/work/ec2_terraform_config_remote_exec/ec2_terraform_config_remote_exec/prv_key.ppk")
+    #agent = true
+    timeout = "4m"
+  }
+
+  provisioner "file" {
+    source      = "/home/runner/work/ec2_terraform_config_remote_exec/ec2_terraform_config_remote_exec/install_python.sh"
+    destination = "/tmp/install_python.sh"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "touch hello.txt",
+      "echo helloworld remote provisioner > hello.txt",
+      "echo hello Rashmi >> hello.txt",
+      "sh /tmp/install_python.sh >> /tmp/output_install_python.txt"
+    ]
+  }
 
   vpc_security_group_ids = [
     aws_security_group.aws_ec_sg.id
